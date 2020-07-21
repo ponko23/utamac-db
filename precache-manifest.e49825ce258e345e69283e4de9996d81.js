3eb8a96feb6fc9a9f4a5966b407b0ecb4f6fc73f
ponko23 <null>
2020-07-21 10:01:36 +0900

    Updates

---
+++ precache-manifest.e49825ce258e345e69283e4de9996d81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e331b2a939bcd9b21502a0e2dba6df",
+    "url": "/game-db/utamac-db/index.html"
+  },
+  {
+    "revision": "e07742168cdabb180ccd",
+    "url": "/game-db/utamac-db/static/js/2.33a11860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-db/utamac-db/static/js/2.33a11860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bffdd13e8ad8ca9f108",
+    "url": "/game-db/utamac-db/static/js/main.d534f9b7.chunk.js"
+  },
+  {
+    "revision": "b1c00ab0e783fb59a603",
+    "url": "/game-db/utamac-db/static/js/runtime-main.e9e9982b.js"
+  }
+]);
