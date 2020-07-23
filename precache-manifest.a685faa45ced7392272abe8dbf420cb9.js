bcf5eedee6c9d06a2eec0a54f7fa6564e6567d7d
ponko23 <ponko23@users.noreply.github.com>
2020-07-23 05:11:41 +0000

    deploy: 50dc69bb50a03734f0e8ff20b97b780b0f75b6ea

--- precache-manifest.a685faa45ced7392272abe8dbf420cb9.js
+++ precache-manifest.a685faa45ced7392272abe8dbf420cb9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1fa0b5f258cbd3400d71fb9e244a7538",
+    "revision": "50f3ad8597ac837b6dc97423515972e0",
     "url": "/game-db/index.html"
   },
   {
-    "revision": "b2164114addcd92d6dfa",
+    "revision": "13be03f6287ca6487561",
     "url": "/game-db/static/css/main.e1ae2a75.chunk.css"
   },
   {
-    "revision": "16f95c6df63e3feb4efe",
-    "url": "/game-db/static/js/2.ed9d9083.chunk.js"
+    "revision": "b0968b9366243fe3b6b0",
+    "url": "/game-db/static/js/2.2c962369.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/game-db/static/js/2.ed9d9083.chunk.js.LICENSE.txt"
+    "url": "/game-db/static/js/2.2c962369.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b2164114addcd92d6dfa",
-    "url": "/game-db/static/js/main.02a6a138.chunk.js"
+    "revision": "13be03f6287ca6487561",
+    "url": "/game-db/static/js/main.125de6a8.chunk.js"
   },
   {
     "revision": "fba48b29fd9cdf12895d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-db/precache-manifest.d4f306ac9682309ff135ca27e92b2fe7.js"
+  "/game-db/precache-manifest.a685faa45ced7392272abe8dbf420cb9.js"
 );
 
 self.addEventListener('message', (event) => {
