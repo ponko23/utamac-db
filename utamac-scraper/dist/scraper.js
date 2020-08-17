4dfe7460eee360ab7e890657b5f58971b98664af
ponko23 <1792133+ponko23@users.noreply.github.com>
2020-08-18 08:24:11 +0900

    distファイル一旦削除

--- utamac-scraper/dist/scraper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseUrl = void 0;
-exports.baseUrl = "https://xn--pckua3ipc5705b.gamematome.jp";
-//# sourceMappingURL=scraper.js.map
