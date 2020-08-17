c89cb0d94505a47ea036758cd0e4f1533e2ee8eb
ponko23 <ponko23@users.noreply.github.com>
2020-08-17 08:07:58 +0000

    deploy: 7cb26d074573c937e315b41cbbe96b1a575c71c9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-db/precache-manifest.49a7216c084e20a23040b3e7e09d2000.js"
+  "/game-db/precache-manifest.36455164c5f53bef5daf999aca593c8d.js"
 );
 
 self.addEventListener('message', (event) => {
